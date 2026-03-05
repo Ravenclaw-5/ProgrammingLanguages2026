8b37f04659e4e46e8afa49e5a6b13316f7d84e55
Ravenclaw-5 <129295263+Ravenclaw-5@users.noreply.github.com>
2026-03-05 13:28:45 -0600

    Added Subtraction to the Interpreter

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,33 +1,33 @@
 enum Primitive {
     Add,
+    Subtract,
     Multiply,
-    Number(i32)
+    Number(i32),
 }
 
 fn eval_prim(primitive: &Primitive) -> i32 {
     match primitive {
         Primitive::Number(val) => *val,
-        _ => 0
+        _ => 0,
     }
 }
 
 fn evaluate(primitives: Vec<Primitive>) -> i32 {
     match primitives[0] {
-        Primitive::Add => { eval_prim(&primitives[1]) + 
-            eval_prim(&primitives[2]) },
-        Primitive::Multiply => { eval_prim(&primitives[1]) *
-            eval_prim(&primitives[2]) },
-        Primitive::Number(_) => 0
-    } 
+        Primitive::Add => eval_prim(&primitives[1]) + eval_prim(&primitives[2]),
+        Primitive::Multiply => eval_prim(&primitives[1]) * eval_prim(&primitives[2]),
+        Primitive::Subtract => eval_prim(&primitives[1]) - eval_prim(&primitives[2]),
+        Primitive::Number(_) => 0,
+    }
 }
 
 fn main() {
-
     let mut primitives = Vec::<Primitive>::new();
-    primitives.push(Primitive::Multiply);
-    primitives.push(Primitive::Number(3));
-    primitives.push(Primitive::Number(4));
+    //primitives.push(Primitive::Add);
+    // primitives.push(Primitive::Multiply);
+    primitives.push(Primitive::Subtract);
+    primitives.push(Primitive::Number(99));
+    primitives.push(Primitive::Number(54));
     let result = evaluate(primitives);
     println!("The result is {result}");
-
 }
